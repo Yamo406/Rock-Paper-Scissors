63812ca9c8417eb9952eb9a95d529f8169784954
Yamo406 <93231355+Yamo406@users.noreply.github.com.>
2024-10-21 03:08:17 +0300

    clean up the code

--- app.js
+++ app.js
@@ -1,33 +1,3 @@
-/*
-Console game of Rock paper scissors
-
-Pseudocode of how we are getting this game done.
-
-We need to get a random rock, paper, or scissors from the computer.
-and we store it in a variable
-THEN we prompt the user for an input
-ONCE the user puts in his answer, we compare.
-    IF our variable is rock and user puts in rock THEN
-        output tie
-    ELSEIF our variable is rock and user puts in paper THEN
-        output "You Won!"
-    ELSEIF our variable is rock and user puts in scissors THEN
-        output "You Lose"
-    ELSEIF our variable is paper and user puts in rock THEN
-        output tie
-    ELSEIF our variable is paper and user puts in paper THEN
-        output "You Won!"
-    ELSEIF our variable is paper and user puts in scissors THEN
-        output "You Lose"
-    ELSEIF our variable is scissors and user puts in rock THEN
-        output tie
-    ELSEIF our variable is scissors and user puts in paper THEN
-        output "You Won!"
-    ELSEIF our variable is scissors and user puts in scissors THEN
-        output "You Lose"
-    ENDIF
-*/
-
 let choices = ["rock", "paper", "scissors"];
 let computerSelection;
 let humanSelection;
@@ -48,8 +18,8 @@ function getComputerChoice()
             break;
         default:
             return "error in the randomNumber";
-        }
     }
+}
 
 function getHumanChoice()
 {
@@ -109,11 +79,8 @@ function playGame()
         console.log(`Your Score: "${humanScore}" - Computer's Score: "${computerScore}"`);
     }
 
-    // round start
-
-    // make a counter that keeps playing the game until there is a winner
-    // the winner must win BO5
-    while (humanScore != 5 && computerScore != 5) {
+    while (humanScore != 5 && computerScore != 5)
+    {
         computerSelection = getComputerChoice();
         humanSelection = getHumanChoice();
         playRound(humanSelection, computerSelection);
